c1ffaeaf59ff28a814824083fa672c76e4b038d0
Miroslau <mira2408@mail.ru>
2020-09-29 12:07:34 +0300

    feat: resolved fun avg all tests passes

--- src/index.js
+++ src/index.js
@@ -36,5 +36,17 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length !== 0){
+    if(array.length === 0) return 0;
+
+    let sum = 0;
+    for(let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+
+    return sum / array.length;
+
+  } else {
+    return 0;
+  }
 }
